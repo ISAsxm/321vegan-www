dd11b8cefd62e63932cfad4d3e1ac1e1045cbf56
ISAsxm <47738803+ISAsxm@users.noreply.github.com>
2025-11-03 16:50:00 -0400

    feat(send email): Added non well known service configuration to api route

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -7,19 +7,34 @@ import EmailTemplate from "@/app/ui/contact/EmailTemplate";
 
 export async function POST(request: NextRequest) {
   const SMTP_SERVER_SERVICE = process.env.SMTP_SERVER_SERVICE;
+  const SMTP_SERVER_HOST = process.env.SMTP_SERVER_HOST;
+  const SMTP_SERVER_PORT = process.env.SMTP_SERVER_PORT;
   const SMTP_SERVER_USERNAME = process.env.SMTP_SERVER_USERNAME;
   const SMTP_SERVER_PASSWORD = process.env.SMTP_SERVER_PASSWORD;
   const WEBSITE_MAIL_RECIEVER = process.env.WEBSITE_MAIL_RECIEVER;
   const { first_name, last_name, email, subject, message } =
     await request.json();
 
-  const transport = nodemailer.createTransport({
-    service: SMTP_SERVER_SERVICE,
-    auth: {
-      user: SMTP_SERVER_USERNAME,
-      pass: SMTP_SERVER_PASSWORD,
-    },
-  });
+  let transport: nodemailer.Transporter;
+  if (SMTP_SERVER_SERVICE) {
+    transport = nodemailer.createTransport({
+      service: SMTP_SERVER_SERVICE,
+      auth: {
+        user: SMTP_SERVER_USERNAME,
+        pass: SMTP_SERVER_PASSWORD,
+      },
+    });
+  } else {
+    transport = nodemailer.createTransport({
+      host: SMTP_SERVER_HOST,
+      port: parseInt(<string>SMTP_SERVER_PORT, 10) || undefined,
+      secure: false,
+      auth: {
+        user: SMTP_SERVER_USERNAME,
+        pass: SMTP_SERVER_PASSWORD,
+      },
+    });
+  }
 
   try {
     await transport.verify();
