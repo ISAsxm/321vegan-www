29cfd7a6db1bc2d44b3618247b37ac46d40c764a
ISAsxm <47738803+ISAsxm@users.noreply.github.com>
2025-11-03 15:25:28 -0400

    fix(testimonials): Fixed responsive bugs

--- app/ui/home/Testimonials.tsx
+++ app/ui/home/Testimonials.tsx
@@ -2,32 +2,36 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { useState, useEffect } from "react";
 import { testimonialData } from "@/assets/assets";
 import Testimonial from "@/app/ui/home/Testimonial";
 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  initialSlide: 0,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 1024, // max-width
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-      },
-    },
-  ],
-};
-
 const Testimonials = () => {
+  const [slidesToShow, setSlidesToShow] = useState(1);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: slidesToShow,
+    slidesToScroll: slidesToShow,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width < 1024) setSlidesToShow(1);
+      else setSlidesToShow(2);
+    };
+
+    handleResize(); // run once on mount
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Slider {...settings} className="mt-12">
       {testimonialData.map((testimonial, i) => (
