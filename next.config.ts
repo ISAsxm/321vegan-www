c4b1d6ede425a0981d21c3a869088a59b44c04ba
ISAsxm <47738803+ISAsxm@users.noreply.github.com>
2025-11-03 06:29:15 -0400

    chore: Added global configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,54 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  allowedDevOrigins: [
+    "localhost",
+    "*.localhost",
+    "192.168.1.5",
+    "*.192.168.1.5",
+  ],
+  serverExternalPackages: ["@react-email/components"],
+  async rewrites() {
+    return [
+      {
+        source: "/a-propos",
+        destination: "/about",
+      },
+      {
+        source: "/scanner-vegan",
+        destination: "/scan",
+      },
+      {
+        source: "/verification-d-additifs",
+        destination: "/additives",
+      },
+      {
+        source: "/verification-de-cosmetiques",
+        destination: "/cosmetics",
+      },
+      {
+        source: "/partenaires",
+        destination: "/partners",
+      },
+      {
+        source: "/suivi-d-impact",
+        destination: "/impact",
+      },
+      {
+        source: "/sources",
+        destination: "/impact",
+      },
+      {
+        source: "/politique-de-confidentialite",
+        destination: "/privacy-policy",
+      },
+      {
+        source: "/mentions-legales",
+        destination: "/terms",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
